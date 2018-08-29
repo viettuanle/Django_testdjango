2b91f08a2f69759d59d5bd9c5d0e0d12c929bca6
TuanLe <vietle.tuan@gmail.com>
2018-08-29 14:40:19 -0700

    reupload to github

--- testdjango/settings.py
+++ testdjango/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = '3jx&v1w^g^%u-plkt(!=r)mgawh%t7o^@jq!@0pjdnebcmz0mi'
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
